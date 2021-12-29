803a6091a3413c58dba91f18df82a7b348cc75d5
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2021-12-29 20:15:50 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -647,16 +647,16 @@ def main():
     donate_handler = CommandHandler("donate", donate)
     migrate_handler = MessageHandler(Filters.status_update.migrate, migrate_chats)
 
-    # dispatcher.add_handler(test_handler)
-    dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(help_handler)
-    dispatcher.add_handler(settings_handler)
-    dispatcher.add_handler(help_callback_handler)
-    dispatcher.add_handler(settings_callback_handler)
-    dispatcher.add_handler(migrate_handler)
-    dispatcher.add_handler(donate_handler)
+    # dispatcher.add_handler(test_handler, run_async=true)
+    dispatcher.add_handler(start_handler, run_async=true)
+    dispatcher.add_handler(help_handler, run_async=true)
+    dispatcher.add_handler(settings_handler, run_async=true)
+    dispatcher.add_handler(help_callback_handler, run_async=true)
+    dispatcher.add_handler(settings_callback_handler, run_async=true)
+    dispatcher.add_handler(migrate_handler, run_async=true)
+    dispatcher.add_handler(donate_handler, run_async=true)
 
-    dispatcher.add_error_handler(error_callback)
+    dispatcher.add_error_handler(error_callback, run_async=true)
 
     if WEBHOOK:
         LOGGER.info("Using webhooks.")
